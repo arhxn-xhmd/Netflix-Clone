bafb071840074ecc79488298c85df0f04895b52f
arhaan <ahmedarhaan732@gmail.com>
2025-05-18 17:10:36 +0530

    first commit first commit

---
+++ Practice/script.js
@@ -0,0 +1,18 @@
+// let number = prompt("Enter a number")
+// let array = []
+
+// for (let index = 1; index <= number; index++) {
+//     array.push(index)   
+// }
+
+// const factorial = (num1, num2) => {
+//     return num1 * num2
+// }
+
+// alert(`The factorial of ${number} is ${array.reduce(factorial)}`)
+
+
+// e = document.body.getElementsByTagName("div");
+// e[4].matches("#redbox")
+
+console.log(document.body)
